dd04e20fae9708109beb6ce661f22a16f65db754
Attila Dekany <dekanyattila94@gmail.com>
2024-01-31 22:52:13 +0100

    feat: add generated client (swagger codegen)

---
+++ src/typescript-angular-client-generated/api/api.ts
@@ -0,0 +1,3 @@
+export * from './hackerNews.service';
+import { HackerNewsService } from './hackerNews.service';
+export const APIS = [HackerNewsService];
---
+++ src/typescript-angular-client-generated/model/models.ts
@@ -0,0 +1,3 @@
+export * from './item';
+export * from './updates';
+export * from './user';
